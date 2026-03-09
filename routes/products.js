424e062aff8891a27a4e86b57d1ba98377b439aa
AnushaReddi02 <anushareddi115@gmail.com>
2026-03-09 20:02:19 +0530

    Fixed SweetAlert conditions in login page for protecting products

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../Models/Product");
 
+// Middleware to protect routes
+function isLoggedIn(req, res, next) {
+  if (!req.session.userId) {
+    return res.redirect("/login?error=loginRequired");
+  }
+  next();
+}
+
 
 // Product List
-router.get("/products", async (req, res) => {
+router.get("/products", isLoggedIn, async (req, res) => {
 
     const products = await Product.find({
         organizationId: req.session.organizationId
@@ -16,26 +24,35 @@ router.get("/products", async (req, res) => {
 
 
 // New Product Form
-router.get("/products/new", (req, res) => {
+router.get("/products/new", isLoggedIn, (req, res) => {
     res.render("products/new");
 });
 
 
 // Create Product
-router.post("/products", async (req, res) => {
+router.post("/products", isLoggedIn, async (req, res) => {
 
-    const product = new Product(req.body.product);
-    product.organizationId = req.session.organizationId;
+    try {
 
-    await product.save();
+        const product = new Product(req.body.product);
+        product.organizationId = req.session.organizationId;
 
-    res.redirect("/products");
+        await product.save();
+
+        res.redirect("/products?success=productCreated");
+
+    } catch(err) {
+
+        console.log(err);
+        res.redirect("/products?error=productCreateFailed");
+
+    }
 
 });
 
 
 // Edit Product Form
-router.get("/products/:id/edit", async (req, res) => {
+router.get("/products/:id/edit", isLoggedIn, async (req, res) => {
 
     const product = await Product.findById(req.params.id);
 
@@ -45,23 +62,40 @@ router.get("/products/:id/edit", async (req, res) => {
 
 
 // Update Product
-router.post("/products/:id", async (req, res) => {
+router.post("/products/:id", isLoggedIn, async (req, res) => {
 
-    await Product.findByIdAndUpdate(req.params.id, req.body.product);
+    try {
 
-    res.redirect("/products");
+        await Product.findByIdAndUpdate(req.params.id, req.body.product);
+
+        res.redirect("/products?success=productUpdated");
+
+    } catch(err) {
+
+        console.log(err);
+        res.redirect("/products?error=productUpdateFailed");
+
+    }
 
 });
 
 
 // Delete Product
-router.post("/products/:id/delete", async (req, res) => {
+router.post("/products/:id/delete", isLoggedIn, async (req, res) => {
 
-    await Product.findByIdAndDelete(req.params.id);
+    try {
 
-    res.redirect("/products");
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.redirect("/products?success=productDeleted");
+
+    } catch(err) {
+
+        console.log(err);
+        res.redirect("/products?error=productDeleteFailed");
+
+    }
 
 });
-
 
 module.exports = router;
